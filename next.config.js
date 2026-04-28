c74b91f3de3c9a79d181d665e9ebdc9857d467a8
gallebaba-glitch <gallebaba@gmail.com>
2026-04-28 18:09:45 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [{ source: '/manifest.json', headers: [{ key: 'Content-Type', value: 'application/manifest+json' }] }]
+  }
 }
 module.exports = nextConfig
