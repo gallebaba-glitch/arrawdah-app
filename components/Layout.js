2f1c401450849c6cb66d56630f1dd227053cf37b
gallebaba-glitch <gallebaba@gmail.com>
2026-05-04 00:24:32 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -53,7 +53,7 @@ export default function Layout({ children, title = '', action }) {
               <img
                 src="/logo-ar-rawdah.png"
                 alt="Ar Rawdah"
-                className="h-12 w-auto object-contain"
+                className="h-20 w-auto object-contain"
                 style={{ filter: 'brightness(0) invert(1)' }}
                 onError={(e) => { e.target.style.display = 'none' }}
               />
